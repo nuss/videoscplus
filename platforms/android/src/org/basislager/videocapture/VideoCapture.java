d56ed74e84241e7d62740e19d8adfbc872c753e7
Stefan Nussbaumer <st9fan@gmail.com>
2015-01-29 21:33:10 +0100

    start working on VideoCapture plugin
    ... swimming somewhere in the middle of the ocean :\
    
    Signed-off-by: Stefan Nussbaumer <st9fan@gmail.com>

---
+++ platforms/android/src/org/basislager/videocapture/VideoCapture.java
@@ -0,0 +1,54 @@
+package org.basislager.videocapture;
+
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.content.Context;
+import android.hardware.camera2.CameraManager;
+
+public class VideoCapture extends CordovaPlugin {
+	private static Context context;
+	private static final CameraManager manager = (CameraManager) context
+			.getSystemService(Context.CAMERA_SERVICE);
+
+	/* constructor */
+	public VideoCapture() {
+	}
+
+	public boolean execute(String action, JSONArray args,
+			CallbackContext callbackContext) throws JSONException {
+		try {
+			if (action.equals("startCapture")) {
+				startCapture(args.getInt(0), callbackContext);
+			} else if (action.equals("stopCapture")) {
+				stopCapture(args.getInt(0), callbackContext);
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			callbackContext.error(e.getMessage());
+		}
+
+		return true;
+	}
+
+	private void startCapture(final int deviceID,
+			final CallbackContext callbackContext) {
+	}
+
+	private void stopCapture(final int deviceID,
+			final CallbackContext callbackContext) {
+	}
+}
