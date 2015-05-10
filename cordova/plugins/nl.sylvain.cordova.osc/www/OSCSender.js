eabbd32c5d5c0905fff2da8b50648e6622f3212a
Stefan Nussbaumer <st9fan@gmail.com>
2015-05-10 20:35:32 +0200

    und weiter im blindflug...
    ... nix geht mehr richtig: OSC? VideoCapture? pfff...
    
    Signed-off-by: Stefan Nussbaumer <st9fan@gmail.com>

---
+++ cordova/plugins/nl.sylvain.cordova.osc/www/OSCSender.js
@@ -0,0 +1,29 @@
+
+var OSCSender = function(host, port){
+	this.host = host;
+	this.port = port;
+}
+
+OSCSender.prototype.send = function(address, data, successCallback, errorCallback){
+
+	//this could be more advanced with checking if no data given, just callbacks etc
+
+	if(typeof data == 'undefined' || !data) data = [];
+	if(!(data instanceof Array)) data = [data];
+
+	//we prepend it so reverse order
+	data.unshift(address);
+	data.unshift(this.port);
+	data.unshift(this.host);
+
+	cordova.exec(successCallback, errorCallback, "OSC", "sendMessage", data);
+}
+
+OSCSender.prototype.close = function(successCallback){
+	cordova.exec(successCallback, function(err){
+		console.log(err);
+	}, "OSC", "closeSender", [this.host, this.port]);
+}
+
+
+module.exports = OSCSender;
